a3f39e4b12522a6b09da1a398ed436877fbdb10e
amon-cofie <ddd.amon123@gmail.com>
2023-02-28 23:32:41 +0000

    set up rails_react app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/configureStore';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById('root'),
+);
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Greeting.create(greeting: "Greeting 1: Hello Monday")
+Greeting.create(greeting: "Greeting 2: Hello Tuesday")
+Greeting.create(greeting: "Greeting 3: Hello Wednesday")
+Greeting.create(greeting: "Greeting 4: Hello Thursday")
+Greeting.create(greeting: "Greeting 5: Hello Friday")
